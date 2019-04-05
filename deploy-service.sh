2fdb14011953790a1b137061563afba308f647e6
Matthew Coleman <matthewdecoleman@gmail.com>
2019-04-05 16:30:50 +0100

    Updated: service deployment and added parameters support

--- deploy-service.sh
+++ deploy-service.sh
@@ -2,4 +2,17 @@
 
 STACK_NAME=$1
 
-aws cloudformation deploy  --capabilities CAPABILITY_NAMED_IAM --stack-name $STACK_NAME --template-file service.yaml
+echo 'Checking if stack exists...'
+if ! aws cloudformation describe-stacks --stack-name $1 ; then
+    echo 'Creating stack'
+    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM \
+                               --stack-name $STACK_NAME \
+                               --template-body file://./service.yaml \
+                               --parameters file://./service.parameters.json
+else
+    echo 'Updating stack'
+    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM \
+                               --stack-name $STACK_NAME \
+                               --template-body file://./service.yaml \
+                               --parameters file://./service.parameters.json
+fi
